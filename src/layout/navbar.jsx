8ef1ec0e272f5a4e69ed970416d664f07fd43321
Huyhoang23231 <tahuyhoang860@gmail.com>
2025-07-16 14:27:49 +0700

    Update Link CV

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -30,7 +30,7 @@ export const Navbar = () => {
             <span className="w-56 h-48 rounded bg-black absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
             <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
               <a
-                href="https://drive.google.com/file/d/1EGHQHpWCRv56Kc774z13eiOIYl3bXlR5/view?usp=sharing"
+                href="https://drive.google.com/file/d/1XfCjdQN1Tu3kIVUSmg-tiPusMAkivTzX/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
               >
